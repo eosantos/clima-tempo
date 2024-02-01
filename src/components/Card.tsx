d678449e8cbae6be5ba93ac8a00c7dcc92938996
eduardooliveira <eos.eduardooliveira@gmail.com>
2024-02-01 20:40:27 -0300

    Refactoring of the card component and creation of the EstiloDados component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,10 @@
 // Card.tsx
 import React from "react";
 import styled from "styled-components";
-import Texto from "./Paragraph/Texto";
-import DadosMeteorologicos from "./DadosMeteorologicos";
-import Alerta from "./Alerta";
-import useClima from "@/hooks/useClima";
 import HeaderCard from "./HeaderCard";
 import Informativo from "./Informativo";
+import DadosMeteorologicos from "./DadosMeteorologicos";
+import Alerta from "./Alerta";
 
 const CardContainer = styled.div`
   max-width: 290px;
@@ -24,104 +22,21 @@ const CardWrapper = styled.div`
   flex: 1;
 `;
 
-const RowContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  position: relative;
-  padding: 5px 0;
-  border-bottom: 1px solid rgba(156, 156, 156, 0.2);
-`;
-
-const TextContainer = styled.div`
-  width: 100px;
-  text-align: left;
-  padding-left: 8px;
-`;
-
-const DadosContainer = styled.div`
-  width: 30;
-  display: flex;  
-`;
-
 const Card: React.FC = () => {
-  const { climaData } = useClima();
 
   return (
     <CardContainer>
       <CardWrapper>
-        {climaData && (
-          <>           
-            <HeaderCard />                        
-            <Informativo />
-            <RowContainer>
-              <TextContainer>
-                <Texto text="Temperatura:" color="#9c9c9c" />
-              </TextContainer>
-              <DadosContainer>
-                <DadosMeteorologicos
-                  text={`${climaData.data[0].temperature.min}%`}
-                  $textColorOption="color1"
-                  $cardColorOption="color1"
-                  $stripeColorOption="color1"
-                />
-                <DadosMeteorologicos
-                  text={`${climaData.data[0].temperature.max}%`}
-                  $textColorOption="color2"
-                  $cardColorOption="color2"
-                  $stripeColorOption="color2"
-                />
-              </DadosContainer>
-            </RowContainer>
+        <HeaderCard />
+        <Informativo />
+        <DadosMeteorologicos />
 
-            <RowContainer>
-              <TextContainer>
-                <Texto text="Umidade:" color="#9c9c9c" />
-              </TextContainer>
-              <DadosContainer>
-                <DadosMeteorologicos
-                  text={`${climaData.data[0].humidity.min}% - ${climaData.data[0].humidity.max}%`}
-                  $textColorOption="color3"
-                  $cardColorOption="color3"
-                  $stripeColorOption="color3"
-                />
-              </DadosContainer>
-            </RowContainer>
-
-            <RowContainer>
-              <TextContainer>
-                <Texto text="Sol:" color="#9c9c9c" />
-              </TextContainer>
-              <DadosContainer>
-                <DadosMeteorologicos
-                  text={`${climaData.data[0].sun.sunrise} - ${climaData.data[0].sun.sunset}`}
-                  $textColorOption="color4"
-                  $cardColorOption="color4"
-                  $stripeColorOption="color4"
-                />
-              </DadosContainer>
-            </RowContainer>
-
-            <RowContainer>
-              <TextContainer>
-                <Texto text="Chuva:" color="#9c9c9c" />
-              </TextContainer>
-              <DadosContainer>
-                <DadosMeteorologicos
-                  text={`${climaData.data[0].rain.probability} %`}
-                />
-              </DadosContainer>
-            </RowContainer>
-
-            <Alerta
-              text="Temperatura deste dia será maior do que a média do período"
-              $textColorOption="color1"
-              $cardColorOption="color1"
-              $stripeColorOption="color1"
-            />
-          </>
-        )}
+        <Alerta
+          text="Temperatura deste dia será maior do que a média do período"
+          $textColorOption="color1"
+          $cardColorOption="color1"
+          $stripeColorOption="color1"
+        />
       </CardWrapper>
     </CardContainer>
   );
--- src/components/DadosMeteorologicos.tsx
+++ src/components/DadosMeteorologicos.tsx
@@ -1,88 +1,100 @@
 import styled from "styled-components";
+import Texto from "./Paragraph/Texto";
+import EstiloDados from "./EstiloDados";
+import useClima from "@/hooks/useClima";
 
-interface DadosMeteorologicosProps {
-  text: string;
-  $textColorOption?: "color1" | "color2" | "color3" | "color4";
-  $cardColorOption?: "color1" | "color2" | "color3" | "color4";
-  $stripeColorOption?: "color1" | "color2" | "color3" | "color4";
-}
-
-const getColorByOption = (
-  option: "color1" | "color2" | "color3" | "color4"
-): string => {
-  switch (option) {
-    case "color1":
-      return "#58aed5";
-    case "color2":
-      return "#f18d88";
-    case "color3":
-      return "#439c99";
-    case "color4":
-      return "#f19631";
-    default:
-      return "#9c9c9c";
-  }
-};
-
-const applyOpacity = (color: string): string => {
-  return `rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(
-    color.slice(3, 5),
-    16
-  )}, ${parseInt(color.slice(5, 7), 16)}, 0.2)`;
-};
-
-const StyledDadosMeteorologicos = styled.p.attrs<{
-  $textColorOption: "color1" | "color2" | "color3" | "color4";
-}>((props) => ({
-  $textColorOption: props.$textColorOption,
-}))`
-  font-size: 8px;
-  color: ${(props) => getColorByOption(props.$textColorOption)};
-  padding: 0px 20px;
+const ContinerWrapper = styled.div`
+  margin: 0;
+  padding: 0;
 `;
 
-const StyledCard = styled.div.attrs<{
-  $cardColorOption: "color1" | "color2" | "color3" | "color4";
-  $stripeColorOption: "color1" | "color2" | "color3" | "color4";
-}>((props) => ({
-  $cardColorOption: props.$cardColorOption,
-  $stripeColorOption: props.$stripeColorOption,
-}))`
-  background: ${(props) =>
-    applyOpacity(getColorByOption(props.$cardColorOption))};
-  border-radius: 3px;
-  padding: 5px 5px;
-  margin: 5px;
+const RowContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
   position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-  width: 3px;
-    background-color: ${(props) => getColorByOption(props.$stripeColorOption)};
-    border-radius: 3px 0 0 3px;
-  }
+  padding: 5px 0;
+  border-bottom: 1px solid rgba(156, 156, 156, 0.2);
 `;
 
-const DadosMeteorologicos: React.FC<DadosMeteorologicosProps> = ({
-  text,
-  $textColorOption = "default",
-  $cardColorOption,
-  $stripeColorOption,
-}) => {
+const TextContainer = styled.div`
+  width: 100px;
+  text-align: left;
+  padding-left: 8px;
+`;
+
+const DadosContainer = styled.div`
+  width: 30;
+  display: flex;  
+`;
+
+const DadosMeteorologicos: React.FC = () => {
+  const { climaData } = useClima();
+
   return (
-    <StyledCard
-      $cardColorOption={$cardColorOption || "default"}
-      $stripeColorOption={$stripeColorOption || "default"}
-    >
-      <StyledDadosMeteorologicos $textColorOption={$textColorOption}>
-        {text}
-      </StyledDadosMeteorologicos>
-    </StyledCard>
+    <ContinerWrapper>
+      {climaData && (
+        <>
+          <RowContainer>
+            <TextContainer>
+              <Texto text="Temperatura:" color="#9c9c9c" />
+            </TextContainer>
+            <DadosContainer>
+              <EstiloDados
+                text={`${climaData.data[0].temperature.min}%`}
+                $textColorOption="color1"
+                $cardColorOption="color1"
+                $stripeColorOption="color1"
+              />
+              <EstiloDados
+                text={`${climaData.data[0].temperature.max}%`}
+                $textColorOption="color2"
+                $cardColorOption="color2"
+                $stripeColorOption="color2"
+              />
+            </DadosContainer>
+          </RowContainer>
+
+          <RowContainer>
+            <TextContainer>
+              <Texto text="Umidade:" color="#9c9c9c" />
+            </TextContainer>
+            <DadosContainer>
+              <EstiloDados
+                text={`${climaData.data[0].humidity.min}% - ${climaData.data[0].humidity.max}%`}
+                $textColorOption="color3"
+                $cardColorOption="color3"
+                $stripeColorOption="color3"
+              />
+            </DadosContainer>
+          </RowContainer>
+
+          <RowContainer>
+            <TextContainer>
+              <Texto text="Sol:" color="#9c9c9c" />
+            </TextContainer>
+            <DadosContainer>
+              <EstiloDados
+                text={`${climaData.data[0].sun.sunrise} - ${climaData.data[0].sun.sunset}`}
+                $textColorOption="color4"
+                $cardColorOption="color4"
+                $stripeColorOption="color4"
+              />
+            </DadosContainer>
+          </RowContainer>
+
+          <RowContainer>
+            <TextContainer>
+              <Texto text="Chuva:" color="#9c9c9c" />
+            </TextContainer>
+            <DadosContainer>
+              <EstiloDados text={`${climaData.data[0].rain.probability} %`} />
+            </DadosContainer>
+          </RowContainer>
+        </>
+      )}
+    </ContinerWrapper>
   );
 };
 
